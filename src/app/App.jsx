68f9adb16a3de7396aec78ce47d3bdc580d2845e
Leo Gomes <106993889+leo-gomes-dev@users.noreply.github.com>
2026-08-31 14:05:26 -0300

    feat: implementa o tarefas length

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import "./App.css";
 
 function App() {
@@ -6,15 +6,17 @@ function App() {
     const tarefasStorage = localStorage.getItem("@leodev");
     return tarefasStorage ? JSON.parse(tarefasStorage) : [];
   });
+
   const [input, setInput] = useState("");
 
+  // Função para adicionar uma tarefa na lista
   const handleAdd = () => {
     if (!input.trim()) return;
     setTarefas([...tarefas, input]);
     setInput("");
   };
 
-  // Função nova para deletar tarefas da lista
+  // Função para deletar tarefas da lista
   const handleDelete = (indexToDelete) => {
     const updatedTarefas = tarefas.filter(
       (_, index) => index !== indexToDelete,
@@ -26,10 +28,19 @@ function App() {
     localStorage.setItem("@leodev", JSON.stringify(tarefas));
   }, [tarefas]);
 
+  const tarefasMemo = useMemo(() => {
+    return tarefas.length;
+  }, [tarefas]);
+
   return (
     <section id="center">
       <div className="todo-card">
         <h1>Minhas Tarefas</h1>
+        <strong className="tarefas-memo">
+          {tarefasMemo
+            ? `Você tem ${tarefasMemo} Tarefa${tarefasMemo > 1 ? "s" : ""}`
+            : "Digite a primeira tarefa"}
+        </strong>
 
         <div className="input-group">
           <input
@@ -43,7 +54,6 @@ function App() {
             {input.trim() ? "Adicionar" : "Bloqueado"}
           </button>
         </div>
-
         <ul className="task-list">
           {tarefas.map((task, index) => (
             <li key={index} className="task-item">
